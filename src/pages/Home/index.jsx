fa36d084982057fca6794ad608109df44de2c545
Leonardo <rimesleo@gmail.com>
2023-04-02 11:20:59 -0300

    tags selecionadas para o filtro

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,6 +13,21 @@ import { api } from "../../services/api";
 
 export function Home() {
     const [tags, setTags] = useState([]);
+    const [tagsSelected, setTagsSelected] = useState([]);
+
+    function handleTagSelected(tagName) {
+        const alreadySelected = tagsSelected.includes(tagName);
+        
+        if(alreadySelected){
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags);
+        }else{
+            setTagsSelected( prevState => [...prevState, tagName]);
+
+        }
+
+    }
+
 
 
 
@@ -38,13 +53,18 @@ export function Home() {
                      <li>
                      <ButtonText
                         title="Todos"
-                        isActive
+                        onClick={() => handleTagSelected("all")}
+                        isActive = {tagsSelected.length ===  0 }
                        />
                     </li>
                     {
                         tags && tags.map(tag => (
                             <li key={String(tag.id)}>
-                                <ButtonText title={tag.name} />
+                                <ButtonText 
+                                title={tag.name}
+                                onClick={() => handleTagSelected(tag.name)}
+                                isActive = {tagsSelected.includes(tag.name)}
+                                />
                             </li>
                         ))
                     }
