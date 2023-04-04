3d8af56f9a9ce6915d16fbfb4b19e42149a64fea
Leonardo <rimesleo@gmail.com>
2023-04-04 06:53:55 -0300

    filtragem de tags e campo search implementados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,10 +12,17 @@ import { api } from "../../services/api";
 
 
 export function Home() {
+    const [search, setSearch] = useState("");
     const [tags, setTags] = useState([]);
     const [tagsSelected, setTagsSelected] = useState([]);
+    const [notes, setNotes] = useState([]);
 
     function handleTagSelected(tagName) {
+
+        if(tagName === "all"){
+            return setTagsSelected([])
+        }
+
         const alreadySelected = tagsSelected.includes(tagName);
         
         if(alreadySelected){
@@ -28,9 +35,6 @@ export function Home() {
 
     }
 
-
-
-
     useEffect(() => {
         async function fetchTags(){
             const response = await api.get("/tags");
@@ -39,6 +43,15 @@ export function Home() {
 
         fetchTags();
     }, [])
+
+    useEffect(() => {
+        async function fetchNotes(){
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+            setNotes(response.data);
+        }
+
+        fetchNotes();
+    }, [tagsSelected, search]);
     return(
         <Container>
             <Brand>
@@ -75,21 +88,24 @@ export function Home() {
             </Menu>
 
             <Search>
-                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+                <Input 
+                     placeholder="Pesquisar pelo título" icon={FiSearch}
+                     onChange={(e) => setSearch(e.target.value)}  
+                />
             </Search>
 
             <Content>
                 <Section title="Minhas Notas">
-                    <Note data={{ 
-                                  title: 'React', 
-                                  tags: [
-                                    {id: '1', name: 'react'},
-                                    {id: '2', name: 'rocketseat'}
-                                
-                                ]
-                                
-                                }}                                  
-                                  />
+                {
+                    notes.map(note => (
+                        <Note
+                            key={String(note.id)}
+                            data={note}
+                        />
+                    ))
+                }
+                  
+                  
                 </Section>
             </Content>
 
